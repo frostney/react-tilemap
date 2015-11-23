10aa525152e63f07de5fa2a1211228810d510d94
Johannes Stein <johannesstein@freeze-dev.com>
2015-11-23 18:48:21 +0100

    First draft

---
+++ index.js
@@ -0,0 +1,62 @@
+import React, {Component, PropTypes} from 'react';
+
+import TileLogic from 'tilelogic';
+
+class Tilemap extends Component {
+  static propTypes = {
+    tileSizeWidth: PropTypes.number,
+    tileSizeHeight: PropTypes.number,
+    width: PropTypes.number,
+    height: PropTypes.number,
+    tiles: PropTypes.oneOfType([PropTypes.array, PropTypes.func]),
+    onRenderTile: PropTypes.func,
+    className: PropTypes.string,
+    style: PropTypes.object,
+  }
+
+  static defaultProps = {
+    tileSizeWidth: 80,
+    tileSizeHeight: 80,
+    width: 10,
+    height: 10,
+    className: '',
+    style: {},
+  }
+
+  constructor(props) {
+    super(props);
+
+    this.tileLogic = new TileLogic(props.width, props.height, this.props.tiles);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    
+  }
+
+  componentShouldUpdate() {
+
+  }
+
+  render() {
+    const tiles = this.tileLogic.map((x, y, type) => {
+      const tileStyle = {
+        width: this.props.tileSizeWidth,
+        height: this.props.tileSizeHeight,
+      };
+
+      const tile = this.props.onRenderTile ? this.props.onRenderTile(x, y, type) : null;
+
+      return (
+        <div className="tile" style={tileStyle}>{tile}</div>
+      );
+    });
+
+    const className = this.props.className;
+
+    return (
+      <div className={className} style={this.props.style}>{tiles}</div>
+    );
+  }
+}
+
+export default Tilemap;
